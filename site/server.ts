8d222b8af2508cfad5bb93195b25c4ea787f1132
jpchapiewsky <jpchapiewsky@gmail.com>
2023-08-14 09:13:30 -0500

    copy output.json to site folder

--- site/server.ts
+++ site/server.ts
@@ -1,21 +1,21 @@
-import { serveDir, serveFile } from "https://deno.land/std@0.194.0/http/file_server.ts";
+import { serveFile } from "https://deno.land/std@0.194.0/http/file_server.ts";
 
 Deno.serve({port: 80}, (req: Request) => {
     const pathname = new URL(req.url).pathname;
 
     if (pathname === "/favicon.ico") {
-        return serveFile(req, "../importer/output.json");
+        return serveFile(req, `${Deno.cwd()}/favicon.ico`);
     }
 
     if (pathname === "/flavors.json") {
-        return serveFile(req, "../importer/output.json");
+        return serveFile(req, `${Deno.cwd()}/output.json`);
     }
 
     if (pathname === "/") {
-        return serveFile(req, "./index.html");
+        return serveFile(req, `${Deno.cwd()}/index.html`);
     }
 
-    return new Response("404: Not Found", {
+    return new Response(`404: The Page (${pathname}) is Not Found`, {
         status: 404,
       });
 });
