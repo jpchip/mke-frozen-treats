9198509a06dce5be0e5ecaeef6f0eb4a71c5f38a
jpchapiewsky <jpchapiewsky@gmail.com>
2023-08-18 14:21:36 -0500

    add roberts and kraverz

---
+++ importer/sites/kraverz.ts
@@ -0,0 +1,21 @@
+/// <reference types="https://deno.land/x/puppeteer@16.2.0/vendor/puppeteer-core/puppeteer/common/Browser.d.ts" />
+import { Browser } from "https://deno.land/x/puppeteer@16.2.0/vendor/puppeteer-core/puppeteer/common/Browser.js";
+import { MkeFrozenTreatsImporter } from "../importer.interface.ts";
+
+export async function load(browser: Browser, site: MkeFrozenTreatsImporter.Site): Promise<string> {
+    console.log('Loading Kraverz...');
+    const page = await browser.newPage();
+    page.emulateTimezone('America/Chicago');
+
+    await page.goto(site.url);
+
+    const flavorEl = await page.waitForSelector(`.CurrentFlavor`, { timeout: 2000 });
+    
+    const flavorOfTheDay = await page.evaluate(
+        // deno-lint-ignore no-explicit-any
+        (flavorEl: any) => flavorEl.textContent,
+        flavorEl
+    );
+
+    return flavorOfTheDay;
+}
