223325271de10b4f07763107a5b0e1a82096c2bb
jpchapiewsky <jpchapiewsky@gmail.com>
2023-08-24 08:29:34 -0500

    remove hard coded url to flavors json, use env variable instead

--- site/server.ts
+++ site/server.ts
@@ -1,4 +1,11 @@
 import { serveFile, serveDir } from "https://deno.land/std@0.194.0/http/file_server.ts";
+import { Eta } from "https://deno.land/x/eta@v3.0.3/src/index.ts"
+import "https://deno.land/std@0.198.0/dotenv/load.ts";
+
+const FLAVORS_OF_THE_DAY_URL = Deno.env.get("FLAVORS_OF_THE_DAY_URL");
+if (FLAVORS_OF_THE_DAY_URL === undefined) {
+  throw new TypeError("Missing FLAVORS_OF_THE_DAY_URL environment variable.");
+}
 
 Deno.serve({port: 80}, (req: Request) => {
     const pathname = new URL(req.url).pathname;
@@ -27,11 +34,18 @@ Deno.serve({port: 80}, (req: Request) => {
       }
 
     if (pathname === "/flavorsOfTheDay.json") {
-        return serveFile(req, `${Deno.cwd()}/output.json`);
+        return serveFile(req, `${Deno.cwd()}/output.json`);      
     }
 
     if (pathname === "/") {
-        return serveFile(req, `${Deno.cwd()}/index.html`);
+        const eta = new Eta({ views: `${Deno.cwd()}/templates` });
+        const res = eta.render("./index", { FLAVORS_OF_THE_DAY_URL: FLAVORS_OF_THE_DAY_URL });
+        return new Response(res, {
+            headers: {
+              "content-type": "text/html; charset=utf-8",
+            },
+          });
+        //return serveFile(req, `${Deno.cwd()}/index.html`);
     }
 
     return new Response(`404: The Page (${pathname}) is Not Found`, {
